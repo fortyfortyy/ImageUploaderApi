df0e79951694e5ca54f421d386372ed5e77d216e
fortyfortyy <d.pacek1@gmail.com>
2023-02-18 00:45:54 +0100

    add custom user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserAccount(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    account_tier = models.ForeignKey("AccountTier", on_delete=models.SET_DEFAULT, default=1)
+
+    def __str__(self):
+        return "%s - %s", self.username, self.account_tier.name
+
+
+class AccountTier(models.Model):
+    name = models.CharField(max_length=100)
+    # thumbnail_sizes = models.ManyToManyField(ThumbnailSize)
+    can_generate_expiring_links = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
