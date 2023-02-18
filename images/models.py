c6a9e58d6e3d86434378c646309a4e67d909a2b2
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 00:14:27 +0100

    feat: imporove the Image, ExpiringLink models
    Image model has been udapted with the custom storage and get_thumbnails func which return the all available paths to the thumbnails based on user account

--- images/models.py
+++ images/models.py
@@ -1,9 +1,12 @@
+import os
 import uuid
 
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 from images.utils import image_upload_path
+from images.validators import validate_image_extension
+from ImageUploadApi.storage import CustomImageStorage
 
 User = settings.AUTH_USER_MODEL  # auth.User
 
@@ -22,11 +25,11 @@ class ThumbnailSize(models.Model):
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=image_upload_path)
+    image = models.ImageField(upload_to=image_upload_path, validators=[validate_image_extension], storage=CustomImageStorage())
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.name} - {self.image.name}"
+        return f"{self.user} - {self.image}"
 
     def __repr__(self):
         return """Image(id='%s', user='%s', image='%s', uploaded_at='%s')""" % (
@@ -36,6 +39,31 @@ class Image(models.Model):
             self.uploaded_at,
         )
 
+    @property
+    def get_orignal_url(self):
+        return self.image.url
+
+    def get_thumbnails(self):
+        user_account_tier = self.user.account_tier
+        available_thumbnail_sizes = user_account_tier.get_available_heights
+        base_file = os.path.dirname(self.image.url)
+
+        directory = os.path.dirname(self.image.path)
+        thumbnails = [f for f in os.listdir(directory)]
+        thumbnails_to_return = []
+        for thumbnail in thumbnails:
+            name, _ = thumbnail.rsplit(".", 1)
+            height = name.split("_")[-1]
+
+            if height.isdigit() and int(height) in available_thumbnail_sizes:
+                path_to_thumbnail = os.path.join(base_file, thumbnail)
+                thumbnails_to_return.append(path_to_thumbnail)
+
+        if user_account_tier.get_original_file:
+            thumbnails_to_return.append(self.image.url)
+
+        return thumbnails_to_return
+
 
 class ExpiringLink(models.Model):
     image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="expiring_links")
@@ -43,11 +71,7 @@ class ExpiringLink(models.Model):
     expiry_time = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.link} - {self.image.name}"
+        return f"{self.link} - {self.image}"
 
     def __repr__(self):
-        return """ExpiringLink(image='%s', link='%s', expiry_time='%s')""" % (
-            self.image.name,
-            self.link,
-            self.expiry_time
-        )
+        return """ExpiringLink(image='%s', link='%s', expiry_time='%s')""" % (self.image, self.link, self.expiry_time)
