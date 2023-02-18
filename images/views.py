d6bfc33d10bf3d52333055ac89f61ae22db0449f
fortyfortyy <d.pacek1@gmail.com>
2023-02-18 23:55:53 +0100

    feat: implement image list view in serialized way

--- images/views.py
+++ images/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from images.models import Image
+from images.serializers import ImageSerializer
+
+
+class ImageListView(generics.ListAPIView):
+    serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        user = None
+        if self.request.user.is_authenticated:
+            user = self.request.user
+
+        return Image.objects.filter(user=user)
