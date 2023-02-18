e6904facf63ed38ecd3fa543c9cd1957e153a054
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 00:15:25 +0100

    feat: implement new url to list all user's images

--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from images import views
 
 urlpatterns = [
-
+    path("", views.ImageListView.as_view(), name="image-list"),
 ]
