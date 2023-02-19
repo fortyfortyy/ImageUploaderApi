96ec85d9daa66bfe9d858270b7b46dde71a1a00a
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 23:44:56 +0100

    feat: create endpoints for expiring links
    add format suffixes to serve media types

--- images/urls.py
+++ images/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from images import views
 
 urlpatterns = [
     path("", views.ImageListView.as_view(), name="image-list"),
-    path("upload/", views.ImageUploadViewSet.as_view(), name="image-upload"),
+    path("upload/", views.ImageCreateView.as_view(), name="image-create"),
+    path("expiring-links/", views.ExpiringLinkListCreateView.as_view(), name='expiring-link-create-list'),
+    path("expiring-links/<str:signed_link>/", views.ExpiringLinkDetailView.as_view(), name='expiring-link-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
