05aadac7d074ac2ccb690d1ab9c69635ae047819
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 23:45:17 +0100

    feat: create new validate_expiration_time validator

--- images/validators.py
+++ images/validators.py
@@ -1,7 +1,8 @@
 import os
 
-from django.conf import settings
 from django.core.exceptions import ValidationError
+
+from default_configs import MAX_EXPIRY_LINK_TIME, MAX_UPLOAD_SIZE, MIN_EXPIRY_LINK_TIME
 
 
 def validate_image_extension(value):
@@ -10,5 +11,10 @@ def validate_image_extension(value):
     if not ext.lower() in valid_extensions:
         raise ValidationError("Unsupported file extension. Please upload a JPG or PNG image.")
 
-    if value.size > settings.MAX_UPLOAD_SIZE:
+    if value.size > MAX_UPLOAD_SIZE:
         raise ValidationError('File size exceeds the limit')
+
+
+def validate_expiration_time(value):
+    if not MIN_EXPIRY_LINK_TIME <= value <= MAX_EXPIRY_LINK_TIME:
+        raise ValidationError(f'Expiration time must be between {MIN_EXPIRY_LINK_TIME} and {MAX_EXPIRY_LINK_TIME} seconds.')
