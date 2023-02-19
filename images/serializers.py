e9bd8d70ad14427828c0b297a0c72ac5ceaa03e6
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 17:20:01 +0100

    feat: implement ImageUploadSerializer

--- images/serializers.py
+++ images/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from images.models import Image
+from images.utils import sanitize_filename
+from images.validators import validate_image_extension
 
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageListSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
@@ -20,3 +22,19 @@ class ImageSerializer(serializers.ModelSerializer):
             return None
 
         return obj.get_thumbnails()
+
+
+class ImageUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('image',)
+
+    def validate_image(self, value):
+        validate_image_extension(value)
+        filename = sanitize_filename(value.name)
+        value.name = filename
+        return value
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return Image.objects.create(**validated_data)
