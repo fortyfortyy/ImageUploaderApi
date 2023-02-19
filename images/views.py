6428cbdfffda7719364446b75d4ef83b6188b84d
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 17:14:37 +0100

    feat: add view to upload image

--- images/views.py
+++ images/views.py
@@ -1,21 +1,23 @@
 from rest_framework import generics
+from rest_framework.parsers import FormParser, MultiPartParser
 
 from images.models import Image
-from images.serializers import ImageSerializer
+from images.serializers import ImageListSerializer, ImageUploadSerializer
 
 
 class ImageListView(generics.ListAPIView):
-    serializer_class = ImageSerializer
+    serializer_class = ImageListSerializer
 
     def get_queryset(self):
-        user = None
-        if self.request.user.is_authenticated:
-            user = self.request.user
-
-        return Image.objects.filter(user=user)
+        return Image.objects.filter(user=self.request.user)
 
 
-# TODO zaimplementować image upload, oraz jego walidacje
+class ImageUploadViewSet(generics.CreateAPIView):
+    queryset = Image
+    serializer_class = ImageUploadSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+
 # TODO zaimplementować wygasające linki
 # TODO przy inicjacji apki od nowa zaimplemenotwać testowego usera
 # TODO zrobić config.py z domyślnymi globalnymi wartościami
