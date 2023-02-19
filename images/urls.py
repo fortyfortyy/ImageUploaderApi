d6c8c51ff16487fb4ea88451d310aba6e0535c3f
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 17:15:02 +0100

    feat: implement new url to upload image

--- images/urls.py
+++ images/urls.py
@@ -4,4 +4,5 @@ from images import views
 
 urlpatterns = [
     path("", views.ImageListView.as_view(), name="image-list"),
+    path("upload/", views.ImageUploadViewSet.as_view(), name="image-upload"),
 ]
