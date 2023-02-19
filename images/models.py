503c250a61af5cad917abfc33fcf98d0e6e04522
fortyfortyy <d.pacek1@gmail.com>
2023-02-19 23:38:09 +0100

    update the models and format code

--- images/models.py
+++ images/models.py
@@ -1,11 +1,12 @@
 import os
+import time
 import uuid
 
 from django.conf import settings
 from django.db import models
 
 from images.utils import image_upload_path
-from images.validators import validate_image_extension
+from images.validators import validate_expiration_time, validate_image_extension
 from ImageUploadApi.storage import CustomImageStorage
 
 User = settings.AUTH_USER_MODEL  # auth.User
@@ -25,7 +26,8 @@ class ThumbnailSize(models.Model):
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=image_upload_path, validators=[validate_image_extension], storage=CustomImageStorage())
+    image = models.ImageField(upload_to=image_upload_path, validators=[validate_image_extension],
+                              storage=CustomImageStorage())
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -46,7 +48,7 @@ class Image(models.Model):
     def get_thumbnails(self):
         user_account_tier = self.user.account_tier
         available_thumbnail_sizes = user_account_tier.get_available_heights
-        base_file = os.path.dirname(self.image.url)
+        base_file = os.path.dirname(self.get_orignal_url)
 
         directory = os.path.dirname(self.image.path)
         thumbnails = [f for f in os.listdir(directory)]
@@ -60,18 +62,26 @@ class Image(models.Model):
                 thumbnails_to_return.append(path_to_thumbnail)
 
         if user_account_tier.get_original_file:
-            thumbnails_to_return.append(self.image.url)
+            thumbnails_to_return.append(self.get_orignal_url)
+
+        if user_account_tier.can_generate_expiring_links and hasattr(self, 'expiring_link'):
+            thumbnails_to_return.append(self.expiring_link.link)
 
         return thumbnails_to_return
 
 
 class ExpiringLink(models.Model):
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="expiring_links")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.OneToOneField(Image, on_delete=models.CASCADE, related_name="expiring_link", unique=True)
     link = models.CharField(max_length=100)
-    expiry_time = models.DateTimeField()
+    expires_in = models.IntegerField(validators=[validate_expiration_time])
 
     def __str__(self):
         return f"{self.link} - {self.image}"
 
     def __repr__(self):
-        return """ExpiringLink(image='%s', link='%s', expiry_time='%s')""" % (self.image, self.link, self.expiry_time)
+        return """ExpiringLink(image='%s', link='%s', expires_in='%s')""" % (self.image, self.link, self.expires_in)
+
+    def is_expired(self):
+        current_time = time.time()
+        return current_time > self.expires_in
