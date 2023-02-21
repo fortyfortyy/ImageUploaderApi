f3d51c2140002d2b71fb2e19c9a8eca5a6bef1cc
fortyfortyy <d.pacek1@gmail.com>
2023-02-21 14:40:58 +0100

    feat: update signals with the celery tasks, add logger

--- images/signals.py
+++ images/signals.py
@@ -1,4 +1,4 @@
-import os
+import logging
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -6,6 +6,8 @@ from django_cleanup.signals import cleanup_pre_delete
 
 from images.models import Image
 from images.tasks import cleanup_image_folder_task, generate_thumbnail_task
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Image)
@@ -16,5 +18,5 @@ def create_thumbnails(sender, instance: Image, **kwargs):
 
 @receiver(cleanup_pre_delete, sender=Image)
 def custom_cleanup_pre_delete(sender, instance, **kwargs):
-    """Signal creates a task in celery to delete image folder """
-    cleanup_image_folder_task.delay(instance.image.path)
+    """Create a task in celery to delete image folder"""
+    cleanup_image_folder_task.delay(instance.image.name)
