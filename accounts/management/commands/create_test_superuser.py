57f171c5083d9865250b5e9edc6bf4677e9185a0
Daniel Pacek <56136594+fortyfortyy@users.noreply.github.com>
2023-02-23 03:33:05 +0100

    Implement Docker-Compose (#7)
    * feat: update requirements.txt
    
    * fix: fix bugs in models where the max length of varchar was set by default to 50 char
    
    * chore: deploy a new script to create custom user admin
    
    * feat: adjust the models with the max_lenght to 255 chars
    
    * chore: update gitignore and dockerignore
    
    * refactor: update settings with env variables
    
    * feat: override get_available_name func to not change the name of the uploading files
    
    * feat: add docker-compose file
    
    * feat: add local configs for docker

---
+++ accounts/management/commands/create_test_superuser.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Create a test user with superuser privileges'
+
+    def handle(self, *args, **options):
+        username = 'testuser'
+        password = 'testpassword'
+        email = 'testuser@example.com'
+        if not get_user_model().objects.filter(username=username).exists():
+            get_user_model().objects.create_superuser(username, email, password)
+            self.stdout.write(self.style.SUCCESS('Successfully created superuser with username "{}" and password "{}".'.format(username, password)))
+        else:
+            self.stdout.write(self.style.WARNING('User "{}" already exists. Please choose a different username.'.format(username)))
