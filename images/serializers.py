9c2c296e8f75ca669ae660d2e68e73bd3e3ec9dc
fortyfortyy <d.pacek1@gmail.com>
2023-03-13 23:44:21 +0100

    fix: fix up the error where any authenticated user could see every user's filenames

--- images/serializers.py
+++ images/serializers.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from images.models import ExpiringLink, Image
 from images.utils import sanitize_filename
@@ -7,15 +8,15 @@ from images.validators import validate_image_extension
 
 
 class ImageListSerializer(serializers.ModelSerializer):
-    images = serializers.SerializerMethodField(read_only=True)
+    image = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Image
         fields = [
-            "images",
+            "image",
         ]
 
-    def get_images(self, obj):
+    def get_image(self, obj):
         thumbnails = obj.get_thumbnails()
         thumbnails_to_return = []
         for thumbnail in thumbnails:
@@ -56,3 +57,16 @@ class ExpiringLinkCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpiringLink
         fields = ('image', 'expires_in')
+
+    def __init__(self, *args, **kwargs):
+        super(ExpiringLinkCreateSerializer, self).__init__(*args, **kwargs)
+
+        # Set the choices based on user's iamges
+        user = self.context.get('request').user
+        images = Image.objects.filter(user=user)
+        self.fields['image'].queryset = images
+
+    def validate_image(self, data):
+        if self.context.get('request').user != data.user:
+            raise ValidationError('You do not have access to this image')
+        return data
