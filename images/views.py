097413c3fc6cf29534cedd19ddb0527f98f1f82d
fortyfortyy <d.pacek1@gmail.com>
2023-03-13 23:46:52 +0100

    refactor: adjust the code and clean up from unnecessary code

--- images/views.py
+++ images/views.py
@@ -1,11 +1,8 @@
 import mimetypes
 
 from django.http import FileResponse
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.exceptions import NotFound, PermissionDenied
-from rest_framework.generics import get_object_or_404
-from rest_framework.parsers import FormParser, MultiPartParser
-from rest_framework.response import Response
 
 from images.mixins import ExpiringLinkMixin
 from images.models import ExpiringLink, Image
@@ -27,33 +24,26 @@ class ImageListView(generics.ListAPIView):
 class ImageCreateView(generics.CreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageCreateSerializer
-    parser_classes = (MultiPartParser, FormParser)
 
 
 class ExpiringLinkListCreateView(generics.ListCreateAPIView, ExpiringLinkMixin):
-    queryset = Image.objects.all()
-    # serializer_class = ExpiringLinkSerializer
-
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return ExpiringLinkCreateSerializer
         return ExpiringLinkListSerializer
 
-    def list(self, request, *args, **kwargs):
-        links = ExpiringLink.objects.filter(image__user=request.user)
-        serializer = self.get_serializer_class()(links, many=True)
-        return Response(serializer.data)
-
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer_class()(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
-        expires_in = request.data.get('expires_in')
-        link = self.generate_expiring_link(validated_data['image'], expires_in)
-        return Response(link, status=status.HTTP_201_CREATED)
+        response = super().create(request, *args, **kwargs)
+        response.data = self.link
+        return response
+
+    def perform_create(self, serializer) -> None:
+        expires_in = self.request.data.get('expires_in')
+        self.link: dict = self.generate_expiring_link(serializer.validated_data['image'], expires_in)
 
     def get_queryset(self):
-        return Image.objects.filter(user=self.request.user)
+        """List expiring links."""
+        return ExpiringLink.objects.filter(image__user=self.request.user)
 
 
 class ExpiringLinkDetailView(generics.RetrieveAPIView, ExpiringLinkMixin):
@@ -63,7 +53,7 @@ class ExpiringLinkDetailView(generics.RetrieveAPIView, ExpiringLinkMixin):
         signed_link = self.kwargs.get('signed_link')
 
         expiring_link_id = self.decode_signed_value(signed_link)
-        expiring_link = get_object_or_404(self.queryset, pk=expiring_link_id)
+        expiring_link = generics.get_object_or_404(self.queryset, pk=expiring_link_id)
         if expiring_link.is_expired():
             expiring_link.delete()
             raise NotFound("Link has expired")
